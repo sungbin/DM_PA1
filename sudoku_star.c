b07747547cbef7b6a95184663287869ea9dbf8d8
sungbin <21700613@handong.edu>
2019-09-24 23:51:21 +0900

    sudoku finally

--- sudoku_star.c
+++ sudoku_star.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #define N 9
-#define f_name "text.txt"
 
 typedef struct _Point {
 	int col;
@@ -100,7 +99,7 @@ main ()
 		Point p1 = star_point[i] ;
 		for(j = 0; j < i; j++) {
 			Point p2 = star_point[j];
-			fprintf(fp,"(not (= p%d%d  p%d%d))",p1.col,p1.row,p2.col,p2.row);
+			fprintf(fp,"(distinct p%d%d  p%d%d)",p1.col,p1.row,p2.col,p2.row);
 		}
 	}
 	fprintf(fp,"))\n") ;
@@ -112,31 +111,38 @@ main ()
 
 	FILE * fin = popen("z3 formula", "r") ; //FIXME
 	char buf[128] ;
+	int new_board[10][10];
 	fscanf(fin, "%s %s", buf, buf) ;
-	while (!feof(fin)) {
-		fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
-		fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
-		fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
-		fscanf(fin, "%s", buf) ; printf("%s ", buf) ;
-		fscanf(fin, "%s", buf) ; printf("%s\n", buf) ;
+	
+	if(strcmp(buf,"(error") == 0) {
+		pclose(fin);
+		printf("No solution\n");
+	} else {
+		while (!feof(fin)) {
+			fscanf(fin, "%s", buf) ; 
+			fscanf(fin, "%s", buf) ; i = buf[1] - '0'; j = buf[2] - '0';
+			fscanf(fin, "%s", buf) ; 
+			fscanf(fin, "%s", buf) ;
+			fscanf(fin, "%s", buf) ; n = buf[0] - '0';
+			new_board[i][j] = n;
+		}
+		pclose(fin) ;
+		for (i = 1 ; i <= 9 ; i++) {
+			for (j = 1 ; j <= 9 ; j++) {
+				printf("%d ", new_board[i][j]) ;
+			}
+			printf("\n") ;
+		}
 	}
-	pclose(fin) ;
 }
 void init_board() {// [1-9, 0, -1] = [num, ?, *]
-	FILE* f;
-	f = fopen(f_name,"r");
-	if(f == NULL) {
-		fprintf(stderr,"file open error\n");
-		exit(1);
-	}
 	int c;
-
 	int cnt = 0;
 	do {
-		c = getc(f);
+		c = getchar();
 		int col = cnt / N;
 		int row = cnt % N;
-		getc(f);
+		getchar();
 
 		if(c == ' ' || c == '\t') {
 			continue;
